38a0a590e879954306ab6890ab07a6ccc1556019
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-03-07 14:47:32 +0100

    my implementation of min_max

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "extent.h"
+#include "morton.h"
+using namespace std;
+//the quadtree is a tree. Root is a square contiaining all the space. Each parent branch has 4 children.
+// If there are twoo many particles in a leaf, branch it.
+void extent(const int N, const float∗ const x, const float∗ const y, float& xmin, float& ymin, float& ext);
+
+int main() {
+
+}
+
---
+++ morton.h
@@ -0,0 +1,9 @@
+//
+// Created by giovanni on 07.03.16.
+//
+#ifndef EX2_CLION_MORTON_H
+#define EX2_CLION_MORTON_H
+
+
+
+#endif //EX2_CLION_MORTON_H
