30bc740463fac5e8afe81d14b60176445c2ff97f
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-03-08 11:27:19 +0100

    before trying to get iterator of array

--- main.cpp
+++ main.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <random>
+#include <assert.h>
 #include "extent.h"
 #include "morton.h"
 #include "profiler.h"
+#include "sort.h"
 //TOME the quadtree is a tree. Root is a square containing all the space. Each parent branch has 4 children.
 // If there more then k particles in a leaf, branch it.
 void InitializeAtRandom(float* x,float* y,int N);
 
 int main() {
+    assert(sizeof(int)==4);
     const int N=1e5;
     float *x=new float[N];
     float *y=new float[N];
+    int *index=new int[N];
     InitializeAtRandom(x,y,N);
 
+    float xmin,ymin,ext;
     {
-        float xmin,ymin,ext;
         Profiler("Extend");
         extent(N,x,y,xmin,ymin,ext);
     }
+    {
+        Profiler("Morton");
+        morton(N,x,y,xmin,ymin,ext,index);
+    }
 
 
-    delete[] x; delete[] y;
+    delete[] x; delete[] y; delete[] index;
 }
 
 void InitializeAtRandom(float* x,float* y,int N){
---
+++ profiler.h
@@ -0,0 +1,34 @@
+//
+// Created by giovanni on 07.03.16.
+//
+#ifndef EX2_CLION_PROFILER_H
+#define EX2_CLION_PROFILER_H
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string>
+
+class Profiler{
+public:
+    Profiler(std::string n0="");
+    inline ~Profiler();
+private:
+    std::string name;
+    timespec t0,t1;
+    long int elapsed();
+};
+
+Profiler::Profiler(std::string n0):name(n0){
+        clock_gettime(CLOCK_MONOTONIC, &t0);
+}
+
+inline Profiler::~Profiler() {
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+    std::cout<<"function "<<name<<" took "<<elapsed()<< " ns to complete"<<std::endl;
+}
+
+long int Profiler::elapsed() {
+    return (long int) (t1.tv_nsec-t0.tv_nsec)+(t1.tv_sec-t0.tv_sec)*1e9;
+}
+
+#endif //EX2_CLION_PROFILER_H
